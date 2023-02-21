e17407da5098322f842d5a3adb0bdefadb0a4ff5
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-02-21 19:54:23 +0700

    gdt and gdtr implementation done
    gdt and gdtr implementation done

--- src/gdt.c
+++ src/gdt.c
@@ -10,24 +10,54 @@ struct GlobalDescriptorTable global_descriptor_table = {
     .table = {
         {
             // Null Descriptor
-            // Base = 0x00
-            // Limit = 0x00
-            // Access Byte = 0x0
-            // Flags = 0x0
+            .segment_low = 0x0000,
+            .base_low = 0x0000,
+
+            .base_mid = 0x00,
+            .type_bit = 0x00,
+            .non_system = 0x00,
+            .dpl_bit = 0x00,
+            .present = 0x00,
+            .segment_high = 0x00,
+            .available_bit = 0x00,
+            .code_in_64bit = 0x00,
+            .db_flag = 0x00,
+            .granularity = 0x00,
+            .base_high = 0x00,
         },
         {
             // Kernel code segment
-            // Base = 0x00
-            // Limit = 0xFFFFF
-            // Access Byte = 0x9A
-            // Flags = 0xCF
+            .segment_low = 0xFFFF,
+            .base_low = 0x0000,
+
+            .base_mid = 0x00,
+            .type_bit = 0xA,
+            .non_system = 0x01,
+            .dpl_bit = 0x00,
+            .present = 0x01,
+            .segment_high = 0xF,
+            .available_bit = 0x00,
+            .code_in_64bit = 0x00,
+            .db_flag = 0x01,
+            .granularity = 0x01,
+            .base_high = 0x00,
         },
         {
             // Kernel data segment
-            // Base = 0x00
-            // Limit = 0xFFFFF
-            // Access Byte = 0x92
-            // Flags = 0xCF
+            .segment_low = 0xFFFF,
+            .base_low = 0x0000,
+
+            .base_mid = 0x00,
+            .type_bit = 0x2,
+            .non_system = 0x01,
+            .dpl_bit = 0x00,
+            .present = 0x01,
+            .segment_high = 0xF,
+            .available_bit = 0x00,
+            .code_in_64bit = 0x00,
+            .db_flag = 0x01,
+            .granularity = 0x01,
+            .base_high = 0x00,
         }
     }
 };
@@ -38,6 +68,6 @@ struct GlobalDescriptorTable global_descriptor_table = {
  * From: https://wiki.osdev.org/Global_Descriptor_Table, GDTR.size is GDT size minus 1.
  */
 struct GDTR _gdt_gdtr = {
-    // TODO : Implement, this GDTR will point to global_descriptor_table. 
-    //        Use sizeof operator
+    .size = sizeof(global_descriptor_table) - 1,
+    .address = &global_descriptor_table
 };
