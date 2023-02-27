c14241f3ea3e76a8d1b91d759a4d1c75d6f70771
GoDillonAudris512 <dillon.audris2004@gmail.com>
2023-02-27 21:42:41 +0700

    refactor: add command to makefile, implement protected mode in kernel.c

--- makefile
+++ makefile
@@ -27,6 +27,7 @@ clean:
 kernel:
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/portio.c -o $(OUTPUT_FOLDER)/portio.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/framebuffer.c -o $(OUTPUT_FOLDER)/framebuffer.o
+	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/gdt.c -o $(OUTPUT_FOLDER)/gdt.o
 	@$(ASM) $(AFLAGS) $(SOURCE_FOLDER)/kernel_loader.s -o $(OUTPUT_FOLDER)/kernel_loader.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/kernel.c -o $(OUTPUT_FOLDER)/kernel.o
 	@$(LIN) $(LFLAGS) bin/*.o -o $(OUTPUT_FOLDER)/kernel
--- src/kernel.c
+++ src/kernel.c
@@ -6,11 +6,17 @@
 #include "lib-header/kernel_loader.h"
 
 void kernel_setup(void) {
+    enter_protected_mode(&_gdt_gdtr);
     framebuffer_clear();
-    framebuffer_write(3, 8,  'H', 0, 0xF);
-    framebuffer_write(3, 9,  'a', 0, 0xF);
-    framebuffer_write(3, 10, 'i', 0, 0xF);
-    framebuffer_write(3, 11, '!', 0, 0xF);
-    framebuffer_set_cursor(3, 10);
+    framebuffer_write(11, 35,  'A', 0, 0xF);
+    framebuffer_write(11, 36,  'p', 0, 0xF);
+    framebuffer_write(11, 37, 'a', 0, 0xF);
+    framebuffer_write(11, 38, 'G', 0, 0xF);
+    framebuffer_write(11, 39, 'a', 0, 0xF);
+    framebuffer_write(11, 40, 'K', 0, 0xF);
+    framebuffer_write(11, 41, 'e', 0, 0xF);
+    framebuffer_write(11, 42, 'O', 0, 0xF);
+    framebuffer_write(11, 43, 'S', 0, 0xF);
+    framebuffer_set_cursor(11, 39);
     while (TRUE);
 }
