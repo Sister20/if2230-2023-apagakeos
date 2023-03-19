bec19f7c4baf2eeb8ae883c10e34aeed00d3080b
AustinPardosi <gabrielpardosi26@gmail.com>
2023-03-19 12:25:37 +0700

    Add Keyboard (Testing) Not Final

--- src/kernel.c
+++ src/kernel.c
@@ -7,16 +7,10 @@
 
 void kernel_setup(void) {
     enter_protected_mode(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
     framebuffer_clear();
-    framebuffer_write(11, 35,  'A', 0, 0xF);
-    framebuffer_write(11, 36,  'p', 0, 0xF);
-    framebuffer_write(11, 37, 'a', 0, 0xF);
-    framebuffer_write(11, 38, 'G', 0, 0xF);
-    framebuffer_write(11, 39, 'a', 0, 0xF);
-    framebuffer_write(11, 40, 'K', 0, 0xF);
-    framebuffer_write(11, 41, 'e', 0, 0xF);
-    framebuffer_write(11, 42, 'O', 0, 0xF);
-    framebuffer_write(11, 43, 'S', 0, 0xF);
-    framebuffer_set_cursor(11, 39);
-    while (TRUE);
+    framebuffer_set_cursor(0, 0);
+    while (TRUE) 
+      keyboard_state_activate();
 }
