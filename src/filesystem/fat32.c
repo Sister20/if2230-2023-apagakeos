f148bdb46cbc96a912fce3fa3a4fd16991d80f3b
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-03-20 00:51:20 +0700

    refactor: add comment to improve readability and understandability. minor change to is empty storage

--- src/filesystem/fat32.c
+++ src/filesystem/fat32.c
@@ -2,9 +2,10 @@
 #include "fat32.h"
 #include "../std/stdmem.h"
 
-struct FAT32DriverState driverState;
-struct FAT32FileAllocationTable fatTable;
+/*-----------------------------------------------------------------------------------*/
+/*-------------------------------------CONSTANT--------------------------------------*/
 
+/* signature of file system */
 const uint8_t fs_signature[BLOCK_SIZE] = {
     'C', 'o', 'u', 'r', 's', 'e', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',  ' ',
     'D', 'e', 's', 'i', 'g', 'n', 'e', 'd', ' ', 'b', 'y', ' ', ' ', ' ', ' ',  ' ',
@@ -15,103 +16,99 @@ const uint8_t fs_signature[BLOCK_SIZE] = {
     [BLOCK_SIZE-1] = 'k',
 };
 
-/* -- Driver Interfaces -- */
+/* struct to save the file system driver state */
+struct FAT32DriverState driverState;
 
-/**
- * Convert cluster number to logical block address
- * 
- * @param cluster Cluster number to convert
- * @return uint32_t Logical Block Address
- */
+/*-----------------------------------------------------------------------------------*/
+/*-------------------------------------HELPER----------------------------------------*/
+
+/* return the logical block address where cluster start */
 uint32_t cluster_to_lba(uint32_t cluster) {
     return cluster * CLUSTER_BLOCK_COUNT;
 }
 
-/**
- * Initialize DirectoryTable value with parent DirectoryEntry and directory name
- * 
- * @param dir_table          Pointer to directory table
- * @param name               8-byte char for directory name
- * @param parent_dir_cluster Parent directory cluster number
- */
-void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster);
-
-/**
- * Checking whether filesystem signature is missing or not in boot sector
- * 
- * @return True if memcmp(boot_sector, fs_signature) returning inequality
- */
-bool is_empty_storage(void) {
-    uint8_t *temp = 0;
-    read_blocks(temp,BOOT_SECTOR,1);
-    return memcmp(fs_signature, temp, BLOCK_SIZE) != 0;
-}
-
-/**
- * Create new FAT32 file system. Will write fs_signature into boot sector and 
- * proper FileAllocationTable (contain CLUSTER_0_VALUE, CLUSTER_1_VALUE, 
- * and initialized root directory) into cluster number 1
- */
-void create_fat32(void) {
-    write_blocks(fs_signature, BOOT_SECTOR, 1);
-
-    fatTable.cluster_map[0] = CLUSTER_0_VALUE;
-    fatTable.cluster_map[1] = CLUSTER_1_VALUE;
-    fatTable.cluster_map[2] = FAT32_FAT_END_OF_FILE;
-    write_clusters(fatTable.cluster_map, 1, 1);
-    
-    struct FAT32DirectoryEntry rootSpecial = {
-        .name = {'r','o','o','t'},
-        .attribute = ATTR_SUBDIRECTORY,
-        .user_attribute = UATTR_NOT_EMPTY,
-        .cluster_high = 0x00,
-        .cluster_low = 0x02,
-        .filesize = 0
-    };
-    struct FAT32DirectoryTable rootDir = {
-        .table = {rootSpecial}
-    };
-    write_clusters(rootDir.table, 2, 1);
-}
-
-/**
- * Initialize file system driver state, if is_empty_storage() then create_fat32()
- * Else, read and cache entire FileAllocationTable (located at cluster number 1) into driver state
- */
-void initialize_filesystem_fat32(void) {
-    if (is_empty_storage()) {
-        create_fat32();
-    } else {
-        read_clusters(&driverState.fat_table, 1, 1);
-        read_clusters(&driverState.dir_table_buf, 2, 1);
-    }
-}
-
-/**
- * Write cluster operation, wrapper for write_blocks().
- * Recommended to use struct ClusterBuffer
- * 
- * @param ptr            Pointer to source data
- * @param cluster_number Cluster number to write
- * @param cluster_count  Cluster count to write, due limitation of write_blocks block_count 255 => max cluster_count = 63
- */
+/* write the the content of ptr to cluster from cluster_number to cluster_number+cluster_count */
 void write_clusters(const void *ptr, uint32_t cluster_number, uint8_t cluster_count) {
     write_blocks(ptr, cluster_to_lba(cluster_number), CLUSTER_BLOCK_COUNT*cluster_count);
 }
 
-/**
- * Read cluster operation, wrapper for read_blocks().
- * Recommended to use struct ClusterBuffer
- * 
- * @param ptr            Pointer to buffer for reading
- * @param cluster_number Cluster number to read
- * @param cluster_count  Cluster count to read, due limitation of read_blocks block_count 255 => max cluster_count = 63
- */
+/* read and put the content of cluster from cluster_number to cluster_number+cluster_count into ptr */
 void read_clusters(void *ptr, uint32_t cluster_number, uint8_t cluster_count) {
     read_blocks(ptr, cluster_to_lba(cluster_number), CLUSTER_BLOCK_COUNT*cluster_count);
 }
 
-/* -- CRUD Operation -- */
+/* put the first entry of this directory table with the directory name and its parent cluster number */
+void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster) {
+    // create new entry of directory table, cluster number refer to the parent of this directory
+    struct FAT32DirectoryEntry dirEntry = {
+        .name = {name[0], name[1], name[2], name[3], name[4], name[5], name[6], name[7]},
+        .attribute = ATTR_SUBDIRECTORY,
+        .user_attribute = UATTR_NOT_EMPTY,
+        .cluster_high = parent_dir_cluster >> 16,
+        .cluster_low = parent_dir_cluster,
+        .filesize = 0,
+    };
+
+    // put the entry as the first entry
+    dir_table->table[0] = dirEntry;
+}
+
+/*-----------------------------------------------------------------------------------*/
+/*-----------------------------------INITIALIZER-------------------------------------*/
+
+/* check if current storage is empty (do not have the filesystem signature) */
+bool is_empty_storage(void) {
+    // initiate buffer to contain boot sector content
+    uint8_t temp[BLOCK_SIZE];
+    
+    // read the content of boot sector and put it to temp
+    read_blocks(temp,BOOT_SECTOR,1);
+
+    // compare the buffer with filesystem signature, return true if equal
+    return memcmp(fs_signature, temp, BLOCK_SIZE) != 0;
+}
+
+/* create new file system if storage is empty */
+void create_fat32(void) {
+    // write the file system signature to the boot sector (cluster 0)
+    write_blocks(fs_signature, BOOT_SECTOR, 1);
+
+    // initialize and write FAT to cluster 1
+    driverState.fat_table.cluster_map[0] = CLUSTER_0_VALUE;
+    driverState.fat_table.cluster_map[1] = CLUSTER_1_VALUE;
+    driverState.fat_table.cluster_map[2] = FAT32_FAT_END_OF_FILE;
+    write_clusters(driverState.fat_table.cluster_map, 1, 1);
+    
+    // initialize root directory and write it to cluster 2
+    struct FAT32DirectoryTable rootDir = {
+        .table = {
+            {
+            .name = {'r','o','o','t'},
+            .attribute = ATTR_SUBDIRECTORY,
+            .user_attribute = UATTR_NOT_EMPTY,
+            .cluster_high = 0x00,
+            .cluster_low = 0x02,
+            .filesize = 0 
+            }
+        }
+    };
+    write_clusters(rootDir.table, 2, 1);
+}
+
+/* initialize file system */
+void initialize_filesystem_fat32(void) {
+    // if storage empty then create new file system
+    // else load the FAT to driverState
+    if (is_empty_storage()) {
+        create_fat32();
+    } 
+    else {
+        read_clusters(&driverState.fat_table, 1, 1);
+    }
+}
+
+/*-----------------------------------------------------------------------------------*/
+/*---------------------------------CRUD OPERATION------------------------------------*/
 
 /**
  *  FAT32 Folder / Directory read
@@ -133,6 +130,7 @@ int8_t read_directory(struct FAT32DriverRequest request);
  * @return Error code: 0 success - 1 not a file - 2 not enough buffer - 3 not found - -1 unknown
  */
 int8_t read(struct FAT32DriverRequest request);
+
 
 /**
  * FAT32 write, write a file or folder to file system.
